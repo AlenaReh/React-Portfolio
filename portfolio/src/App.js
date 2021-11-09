825533ee9ac42728a0c57224326b7436a54bc718
Alena Rehberger <alenarehberger92@gmail.com>
2021-11-08 18:10:05 -0600

    Added basic structure

---
+++ portfolio/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+import Header from './components/Header';
+import Project from './components/Project';
+import Footer from './components/Footer';
+
+
+// In our main App component, we are rendering only single instances of Header and Navbar and several instances of Card
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Header />
+      <Project />
+      <Project />
+      <Project />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
