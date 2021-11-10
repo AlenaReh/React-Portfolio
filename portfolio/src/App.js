4f59d10c7c01cba0f725f1d6935512679ad749e2
Alena Rehberger <alenarehberger92@gmail.com>
2021-11-09 22:44:55 -0600

    Added a Resume component

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,23 +1,26 @@
-import React from 'react';
-import Header from './components/Header/Header';
+import React from "react";
+import Header from "./components/Header/Header";
 import Intro from './components/Intro/Intro';
 // import Project from './components/Project/Project';
-import Footer from './components/Footer/Footer';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
+import Footer from "./components/Footer/Footer";
+import Contact from "./components/Contact/Contact";
+import Resume from "./components/Resume/Resume";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 // In our main App component, we are rendering only single instances of Header and Navbar and several instances of Card
 const App = () => {
   return (
     <div>
       <Header />
-      <Intro/>
+      <Intro />
       {/* <Project />
       <Project />
       <Project /> */}
+      <Contact/>
+      <Resume/>
       <Footer />
     </div>
   );
-}
+};
 
 export default App;
---
+++ portfolio/src/components/Contact/Contact.js
@@ -0,0 +1,74 @@
+import React, { useState } from "react";
+import { Container, Form } from "react-bootstrap";
+import Button from "react-bootstrap/Button";
+import './Contact.css'
+
+function Contact() {
+  let [userName, setUserName] = useState("");
+  let [email, setEmail] = useState("");
+  let [text, setText] = useState("");
+
+    const handleInputChange = (e) => {
+    // Getting the value and name of the input which triggered the change
+    const { name, value } = e.target;
+
+    if (name === "email") {
+      setEmail(value);
+    } else if (name === "userName") {
+      setUserName(value);
+    } else {
+      setText(value);
+    }
+    return;
+    };
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    console.log("Button clicked");
+    // alert(`Hello ${userName} ${lastName} from ${company}!`);
+    setUserName("");
+    setEmail("");
+    setText("");
+  };
+
+  return (
+
+    <Container className='contact-container'>
+      <Form className='contact-form'>
+        <Form.Group controlId="form.Name">
+          <Form.Label>Name</Form.Label>
+          <Form.Control className='contact-input'
+            type="text"
+            placeholder="Enter name"
+            onChange={handleInputChange}
+            value={userName}
+            name="userName"
+          />
+        </Form.Group>
+        <Form.Group controlId="form.Email">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control className='contact-input'
+            type="email"
+            placeholder="name@example.com"
+            onChange={handleInputChange}
+            value={email}
+            name="email"
+          />
+        </Form.Group>
+        <Form.Group controlId="form.Textarea">
+          <Form.Label>Message</Form.Label>
+          <Form.Control className='contact-input' as="textarea" rows={3} onChange={handleInputChange} value={text} name="text" />
+        </Form.Group>
+        <Button className='contact-submit'
+          as="input"
+          type="submit"
+          value="Submit"
+          onClick={handleFormSubmit}
+        />{" "}
+      </Form>
+    </Container>
+
+  );
+}
+
+export default Contact;
